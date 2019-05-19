8fe224c9c5bd06c67d09850d313f840d0e540b79
iwut <anddra@kth.se>
2019-05-19 14:30:11 +0200

    lint fix

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const http = require('http').Server(app)
 const io = require('socket.io')(http)
 const winston = require('winston')
 
-const logger = new(winston.Logger)({
+const logger = new (winston.Logger)({
   transports: [
-      new(winston.transports.File)({filename: 'drone.log'})
+    new (winston.transports.File)({ filename: 'drone.log' })
   ]
-});
+})
 
 let drones = []
 let clients = []
@@ -18,11 +18,11 @@ let record = false
 const RAD_CUTOFF = 0.1
 const WOBBLE_THRESHOLD = 5
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html')
 })
 
-app.get('/drones', function(req, res) {
+app.get('/drones', function (req, res) {
   res.json(drones)
 })
 
@@ -32,11 +32,11 @@ const tellClients = (socket, command, message) => {
   })
 }
 
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
   console.log('a user connected', socket.id)
   socket.emit('socket_id', socket.id)
 
-  socket.on('connect_drone', function() {
+  socket.on('connect_drone', function () {
     if (!drones.includes(socket.id)) {
       drones.push(socket.id)
       tellClients(socket, 'connected_drones', drones)
@@ -44,23 +44,23 @@ io.on('connection', function(socket){
     }
   })
 
-  socket.on('record', function(recording) {
+  socket.on('record', function (recording) {
     record = recording
   })
 
-  socket.on('connect_client', function() {
+  socket.on('connect_client', function () {
     !clients.includes(socket.id) && clients.push(socket.id)
     socket.emit('connected_drones', drones)
   })
 
-  socket.on('drone_command', function(drone, command) {
+  socket.on('drone_command', function (drone, command) {
     socket.broadcast.to(drone).emit('command', JSON.parse(command))
     console.log('Command to drone ' + drone + ': ' + command)
   })
 
-  socket.on('tune', function(drone) {
-    droneData.push({drone, data: []})
-    
+  socket.on('tune', function (drone) {
+    droneData.push({ drone, data: [] })
+
     let P = 160
     let I = 0
     let D = 0
@@ -71,7 +71,7 @@ io.on('connection', function(socket){
     const STD_THRESHOLD = 0.01
 
     const I_EPSILON = 0.1
-    
+
     let data
     let pitch
     let roll
@@ -79,26 +79,23 @@ io.on('connection', function(socket){
 
     let status = 'tuneD'
 
-    socket.broadcast.to(drone).emit('command', {P, I, D, type: 'pid', pid_type})
+    socket.broadcast.to(drone).emit('command', { P, I, D, type: 'pid', pid_type })
 
     const intervalId = setInterval(() => {
       data = getDrone(drone).data
-      pitch = data.map(({pitch}) => pitch)
-
+      pitch = data.map(({ pitch }) => pitch)
 
       if (status === 'tuneP') {
         console.log('tuning P, value is', P)
         // roll = data.map(({roll}) => roll)
         // azimuth = data.map(({azimuth}) => azimuth)
-  
-  
-        
+
         let countWobble = 0
         let prevSide = null
-  
+
         pitch.forEach(value => {
           if (value > RAD_CUTOFF || value < -RAD_CUTOFF) {
-           const side = value < 0 ? 'left' : 'right'
+            const side = value < 0 ? 'left' : 'right'
             if (prevSide === null || prevSide !== side) {
               prevSide = side
               countWobble++
@@ -106,24 +103,22 @@ io.on('connection', function(socket){
             }
           }
         })
-        
+
         console.log('wobble count is', countWobble)
-  
+
         if (countWobble >= WOBBLE_THRESHOLD) {
           const oldP = P
           P = Math.floor(P * 0.8)
           console.log('final P IS', P, 'tuned P is', oldP)
-          
+
           status = 'tuneD'
         }
-  
-  
+
         P += P_INCREMENT
-  
       } else if (status === 'tuneD') {
         console.log('tuning D, value is', D)
-        const mean = pitch.reduce((a,b) => a + b , 0) / pitch.length
-        const stdDev = pitch.map(value => (value-mean)*(value-mean)).reduce((a,b) => a+b, 0) / (pitch.length-1)
+        const mean = pitch.reduce((a, b) => a + b, 0) / pitch.length
+        const stdDev = pitch.map(value => (value - mean) * (value - mean)).reduce((a, b) => a + b, 0) / (pitch.length - 1)
 
         console.log('D tune, mean is', mean, 'std is', stdDev)
 
@@ -133,15 +128,11 @@ io.on('connection', function(socket){
         }
 
         D += D_INCREMENT
-
       } else if (status === 'tuneI') {
         console.log('tuning I, value is', I)
-        const mean = pitch.reduce((a,b) =>a+b , 0) / pitch.length
+        const mean = pitch.reduce((a, b) => a + b, 0) / pitch.length
 
         console.log('I tune, mean is', mean)
-
-
-
 
         if (Math.abs(mean) <= I_EPSILON) {
           console.log('I IS', I)
@@ -152,21 +143,20 @@ io.on('connection', function(socket){
       }
 
       getDrone(drone).data = []
-      
 
-      socket.broadcast.to(drone).emit('command', {P, I, D, type: 'pid', pid_type})
+      socket.broadcast.to(drone).emit('command', { P, I, D, type: 'pid', pid_type })
     }, 5000)
   })
 
-  const getDrone = droneId => droneData.find(({drone}) => drone === droneId)
+  const getDrone = droneId => droneData.find(({ drone }) => drone === droneId)
 
-  socket.on('drone_data', function(data) {
+  socket.on('drone_data', function (data) {
     record && logger.info(data)
     getDrone(data.drone) && getDrone(data.drone).data.push(data)
     tellClients(socket, 'drone_data', data)
   })
 
-  socket.on('disconnect', function(){
+  socket.on('disconnect', function () {
     const id = socket.id
     drones = drones.includes(id) ? drones.filter(drone => drone !== id) : drones
     clients = clients.includes(id) ? clients.filter(client => client !== id) : clients
@@ -174,10 +164,9 @@ io.on('connection', function(socket){
       socket.broadcast.to(client).emit('connected_drones', drones)
     })
     console.log('user disconnected')
-    
-  });
-});
+  })
+})
 
-http.listen(3001, function(){
+http.listen(3001, function () {
   console.log('listening on *:3001')
 })
